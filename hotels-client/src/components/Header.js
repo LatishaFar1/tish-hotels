a812ebeee7bd6e15e446f50fe2e8449da82f8e27
Tish Faroul <lfaroul14@gmail.com>
2022-11-11 22:38:55 -0500

    adding calendar - need to work out the kinks

--- hotels-client/src/components/Header.js
+++ hotels-client/src/components/Header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import { FaHotel } from 'react-icons/fa';
 import {SiHotelsdotcom} from 'react-icons/si';
 import {TbPlaneInflight} from 'react-icons/tb';
-import {BiBed} from 'react-icons/bi'
-import {AiOutlineCalendar} from 'react-icons/ai'
+import {BiBed} from 'react-icons/bi';
+import {AiOutlineCalendar} from 'react-icons/ai';
+import { DateRangePicker } from 'react-date-range';
+import { addDays } from 'date-fns';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 function Header() {
+
+  const [date, setDate] = useState({
+    selection: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection'
+    },
+    compare: {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 3),
+      key: 'compare'
+    }
+  })
+
   return (
     <div className='header'>
         <div className='HeaderContainer'>
@@ -17,11 +35,21 @@ function Header() {
 
             <div className='Search'>
               <div className='SearchBox'>
-                <BiBed />
-                <input type='text' className='searchInput' placeholder="let's plan a trip!" />
-                <AiOutlineCalendar />
-                <input type='text' className='searchInput' placeholder='when are we going?' />
-            
+                <BiBed className='SearchIcon'/>
+                <input type='text' className='SearchInput' placeholder="let's plan a trip!" />
+                <AiOutlineCalendar className='SearchIcon' />
+                      <DateRangePicker
+                        className='calendar'
+                        onChange={item => setDate({ ...date, ...item })}
+                        months={1}
+                        minDate={addDays(new Date(), -300)}
+                        maxDate={addDays(new Date(), 900)}
+                        direction="vertical"
+                        scroll={{ enabled: true }}
+                        ranges={[date.selection, date.compare]}
+                      />;
+                <input type='text' className='SearchInput' placeholder='when are we going?' />
+                <button className='SearchButton'>Search</button>
               </div>
             </div>
         </div>
