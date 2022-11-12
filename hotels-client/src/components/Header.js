e0daaf6bde0f883fbb857fbc2fccb8e6dc43f311
Tish Faroul <lfaroul14@gmail.com>
2022-11-11 22:13:52 -0500

    working on header section

--- hotels-client/src/components/Header.js
+++ hotels-client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import { FaHotel } from 'react-icons/fa';
 import {SiHotelsdotcom} from 'react-icons/si';
 import {TbPlaneInflight} from 'react-icons/tb';
+import {BiBed} from 'react-icons/bi'
+import {AiOutlineCalendar} from 'react-icons/ai'
 
 function Header() {
   return (
@@ -10,6 +12,17 @@ function Header() {
             <div className='HeaderBox'>
             <SiHotelsdotcom /> <span>Booking</span>
             <TbPlaneInflight /> <span>Transportation</span>
+            </div>
+
+
+            <div className='Search'>
+              <div className='SearchBox'>
+                <BiBed />
+                <input type='text' className='searchInput' placeholder="let's plan a trip!" />
+                <AiOutlineCalendar />
+                <input type='text' className='searchInput' placeholder='when are we going?' />
+            
+              </div>
             </div>
         </div>
     
